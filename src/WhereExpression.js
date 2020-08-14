cd128062de83bc24a1132b9396589756d1db77ac
sk <eduards.gruberts@gmail.com>
2020-08-14 20:57:32 +0300

    les code

--- src/WhereExpression.js
+++ src/WhereExpression.js
@@ -11,72 +11,31 @@ class WhereExpression{
 	}
 
 	equal(name, constant){
-		console.log(name, constant);
-		this.expressions.push(
-			{
-				'TYPE': 'EQUAL',
-				'NAME': name,
-				'CONSTANT': constant,
-				'DISJUNCTION': this.disjunction
-			}
-		);
-
+		this.expressions.push({'TYPE': 'EQUAL', 'NAME': name, 'CONSTANT': constant, 'DISJUNCTION': this.disjunction});
 		this.disjunction = false;
 		return this;
 	}
 
 	greater(name, constant){
-		this.expressions.push(
-			{
-				'TYPE': 'GREATER',
-				'NAME': name,
-				'CONSTANT': constant,
-				'DISJUNCTION': this.disjunction
-			}
-		);
-
+		this.expressions.push({'TYPE': 'GREATER', 'NAME': name, 'CONSTANT': constant, 'DISJUNCTION': this.disjunction});
 		this.disjunction = false;
 		return this;
 	}
 
 	less(name, constant){
-		this.expressions.push(
-			{
-				'TYPE': 'LESS',
-				'NAME': name,
-				'CONSTANT': constant,
-				'DISJUNCTION': this.disjunction
-			}
-		);
-
+		this.expressions.push({'TYPE': 'LESS', 'NAME': name, 'CONSTANT': constant, 'DISJUNCTION': this.disjunction});
 		this.disjunction = false;
 		return this;
 	}
 
 	greaterEqual(name, constant){
-		this.expressions.push(
-			{
-				'TYPE': 'GREATER_EQUAL',
-				'NAME': name,
-				'CONSTANT': constant,
-				'DISJUNCTION': this.disjunction
-			}
-		);
-
+		this.expressions.push({'TYPE': 'GREATER_EQUAL', 'NAME': name, 'CONSTANT': constant, 'DISJUNCTION': this.disjunction});
 		this.disjunction = false;
 		return this;
 	}
 
 	lessEqual(name, constant){
-		this.expressions.push(
-			{
-				'TYPE': 'LESS_EQUAL',
-				'NAME': name,
-				'CONSTANT': constant,
-				'DISJUNCTION': this.disjunction
-			}
-		);
-
+		this.expressions.push({'TYPE': 'LESS_EQUAL', 'NAME': name, 'CONSTANT': constant, 'DISJUNCTION': this.disjunction});
 		this.disjunction = false;
 		return this;
 	}
